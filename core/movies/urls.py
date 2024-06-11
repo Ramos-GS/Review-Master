a03c63a82df393e60df3d938b742f8b628387425
Erick Cassoli <erick.mcassoli@gmail.com>
2024-06-11 13:39:57 -0300

    feat: update a urls to review edit and review delete

--- core/movies/urls.py
+++ core/movies/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('movies/', views.movie_list, name='movie_list'),
     path('movies/<int:movie_id>/', views.movie_detail, name='movie_detail'),
     path('movies/<int:movie_id>/review/', views.add_review, name='add_review'),
-    path('review/<int:movie_id>/edit/', views.edit_review, name='edit_review'),
-    path('review/<int:movie_id>/delete/', views.delete_review, name='delete_review'),
+    path('review/<int:review_id>/edit/', views.edit_review, name='edit_review'),
+    path('review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
     path('register/', views.register, name='register'),
 ]
